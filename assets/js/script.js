0268f71593a55f463278125c2e2497585d410916
michaelcounseller <michaelcounseller@gmail.com>
2022-09-18 14:51:32 -0500

    created a test run of an event listener click that will create elements and append it to a unordered list with text content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-// const addBtn = document.querySelector('button');
-// const test = document.querySelector('#add-exercise').textContent;
-
-// addBtn.addEventListener('mouseover', () => {
-//     alert('button clicked')
-// })
-
-// console.log(test);
+const addBtn = document.querySelector("button");
+const test = document.querySelector("#add-exercise").textContent;
+const firstCol = document.querySelector("#first-column");
 
 
 
-// console.log(addBtn);
 
-console.log('1')
 
-setTimeout(() => {
-    console.log('2')
-}, 0)
+let createExercise = () => {
+    const listItem = document.createElement("li");
+    const listPara = document.createElement("p");
+    listPara.className = 'exercise-task has-background-primary-light has-text-info p-1'
+    listPara.textContent = "This is a test";
+  
+    listItem.appendChild(listPara);
+    firstCol.appendChild(listItem);
+}
 
-console.log('3')
+addBtn.addEventListener("click", createExercise);
 
+console.log(test);
+
+console.log(addBtn);
