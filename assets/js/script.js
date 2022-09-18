675b9833970193342b116cbfbb4e1cc82db41ed6
michaelcounseller <michaelcounseller@gmail.com>
2022-09-18 06:48:09 -0500

    learning how the DOM works and figuring out how callback functions works. Also added a title to each column for the future

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// const addBtn = document.querySelector('button');
+// const test = document.querySelector('#add-exercise').textContent;
+
+// addBtn.addEventListener('mouseover', () => {
+//     alert('button clicked')
+// })
+
+// console.log(test);
+
+
+
+// console.log(addBtn);
+
+console.log('1')
+
+setTimeout(() => {
+    console.log('2')
+}, 0)
+
+console.log('3')
+
