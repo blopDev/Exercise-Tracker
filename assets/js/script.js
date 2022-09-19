3a56d920a4fa7585e4a9a01165009b8126319ac1
michaelcounseller <michaelcounseller@gmail.com>
2022-09-19 10:37:47 -0500

    made a form field, also learned what Array.from method is, from my understanding array.from method turns iretables and array like objects into an array and has its own set of rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
 const addBtn = document.querySelector("button");
-const test = document.querySelector("#add-exercise").textContent;
+const test = document.querySelector("#add-exercise");
 const firstCol = document.querySelector("#first-column");
-
-
-
-
+const optionCont = document.getElementsByTagName("select");
 
 let createExercise = () => {
-    const listItem = document.createElement("li");
-    const listPara = document.createElement("p");
-    listPara.className = 'exercise-task has-background-primary-light has-text-info p-1'
-    listPara.textContent = "This is a test";
-  
-    listItem.appendChild(listPara);
-    firstCol.appendChild(listItem);
-}
+  const listItem = document.createElement("li");
+  const listPara = document.createElement("p");
+
+  listPara.className =
+    "exercise-task has-background-primary-light has-text-info p-1";
+  listPara.textContent = "This is a test";
+
+  listItem.appendChild(listPara);
+  firstCol.appendChild(listItem);
+};
+
+let createNums = () => {
+  Array.from(optionCont).forEach((x) => { //Array.from makes the array-like object into an actual Array
+    for (let i = 0; i < 100; i++) {
+      const numCont = document.createElement("option");
+      numCont.text = i;
+      x.appendChild(numCont);
+    }
+  });
+};
+
+createNums();
 
 addBtn.addEventListener("click", createExercise);
-
-console.log(test);
-
-console.log(addBtn);
